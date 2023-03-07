71edf6bb895bf8db525352e6c792feb2d52f085d
Vikash <vikash.kumar@nineleaps.com>
2023-03-07 16:46:53 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 // import Demo from './components/Demo';
-import Department from './components/Department';
+// import Department from './components/Department';
+import Classcom from './components/Classcom';
 // let name="nineleaps"
 function App() {
   return (
     <div>
-    <Department level="operation" name="vikash"/>
+    {/* <Department level="operation" name="vikash"/>
+     */}
+
+     <Classcom/>
+
+
+
     {/* <Demo/>
      <h1> My first react app</h1>
      <h3>I am {name}. you are present in {2+2} locations</h3>
---
+++ src/components/Classcom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Classcom extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I am class components</h1>
+      </div>
+    )
+  }
+}
+
+export default Classcom
